15f7f4188c6463a24b961c86f20a51f5fc91a8eb
Alek Kundla <kundla.9@osu.edu>
2018-10-27 19:03:40 -0400

    Made the Navbar a Component, successfully searched for new quotes

--- app.js
+++ app.js
@@ -1,5 +1,74 @@
 'use strict';
 
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '', };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value.toUpperCase() });
+    }
+
+    handleSubmit(event) {
+                
+        // Grabs the root element at which the React elements defined here will be rendered
+        const rootElement = document.getElementById("root");
+
+        // Renders the chosen components at the root element in the HTML
+        ReactDOM.render(<App symbols={[this.state.value]} />, rootElement);
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="Navbar">
+                <nav class="navbar navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#">AA Stocks</a>
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Watchlist</a>
+                            </li>
+                        </ul>
+
+                        <form onSubmit={this.handleSubmit} class="form-inline input-group my-2">
+                            <input type="search" 
+                                    class="form-control remove-right-curves"
+                                    value={this.state.value} 
+                                    onChange={this.handleChange} 
+                                    placeholder="Enter ticker symbol" />
+                            <input type="submit"
+                                   class="btn btn-primary remove-left-curves"
+                                   value="View Quote" />
+                        </form>
+
+                    </div>
+
+                </nav>
+
+                <br />
+            </div>
+        );
+    }
+}
+
+
 // Stock quote with current price, percent change, and option to view full quote
 class SimpleQuote extends React.Component {
     constructor(props) {
@@ -86,9 +155,9 @@ class QuoteList extends React.Component {
             // Note: we add a key prop here to allow react to uniquely identify each
             // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
             this.state.quotes.push(<SimpleQuote key={i} symbol={this.props.symbols[i]} />);
-            
+
             // Adds break tag for spacing between SimpleQuotes
-            this.state.quotes.push(<br />);
+            this.state.quotes.push(<br key={i + this.props.symbols.length} />);
         }
 
         // Returns the rendered quotes inside a div
@@ -96,8 +165,24 @@ class QuoteList extends React.Component {
     }
 }
 
-// The element that will be rendered
-const element = React.createElement;
+// Initializes the navbar and the stocks list
+class App extends React.Component {
+    constructor(props) {
+        // No clue what this does, seesm unnecessary
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        var pagePieces = [
+            <Navbar key={1} />,
+            // TODO: Make container part of QuoteList
+            <div key={2} id="quote_list" class="container"> <QuoteList symbols={this.props.symbols} /> </div>
+        ];
+
+        return pagePieces;
+    }
+}
 
 // Grabs the root element at which the React elements defined here will be rendered
 const rootElement = document.getElementById("root");
@@ -106,4 +191,4 @@ const rootElement = document.getElementById("root");
 var tickerSymbols = ["TSLA", "AMZN", "GOOG", "MSFT"];
 
 // Renders the chosen components at the root element in the HTML
-ReactDOM.render(<QuoteList symbols={tickerSymbols} />, rootElement);
+ReactDOM.render(<App symbols={tickerSymbols} />, rootElement);
