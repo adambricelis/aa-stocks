eea573c7a64ec2120418c22e1983b5e18e596deb
Alek Kundla <kundla.9@osu.edu>
2018-10-27 13:20:21 -0400

    Converted SimpleQuote to a React class

--- react_components.js
+++ react_components.js
@@ -1,8 +1,30 @@
-// Grabs the root element at which the React elements defined here will be rendered
-const rootElement = document.getElementById("root"); 
+class SimpleQuote extends React.Component {
+    render() {
+        return (
+            <div className="simple-quote">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">TSLA (Tesla Inc.)</h5>
+                        <div class="card-text up">
+                            <div class="row">
+                                <div class="col-6">
+                                    $250.00
+                                </div>
+                                <div class="col-6">
+                                    10.00%
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#" class="btn btn-outline-success mt-3">View Full Quote</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
-// Defines an element
-const element = <h1>Hello, world</h1>;
+// Grabs the root element at which the React elements defined here will be rendered
+const rootElement = document.getElementById("root");
 
 // Renders the chosen components at the root element in the HTML
-ReactDOM.render(element, rootElement);
+ReactDOM.render(<SimpleQuote />, rootElement);
