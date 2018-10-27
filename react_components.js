80c97b36c1042793a12399c87d0f71f8cec5e2b0
Adam Lis <adambricelis@gmail.com>
2018-10-27 14:35:03 -0400

    Styling updates for SimpleQuote and navbar

--- react_components.js
+++ react_components.js
@@ -10,7 +10,7 @@ class SimpleQuote extends React.Component {
             error: null,
             isLoaded: false,
             quoteData: [],
-            textClass: null
+            textClass: ""
         };
     }
 
@@ -39,19 +39,24 @@ class SimpleQuote extends React.Component {
     }
 
     render() {
-        const { error, isLoaded, quoteData, textClass } = this.state;
+        const { error, isLoaded, quoteData } = this.state;
 
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
             return <div>Loading...</div>;
         } else {
+            if (quoteData.changePercent >= 0) {
+                this.state.textClass = "card-text up";
+            } else {
+                this.state.textClass = "card-text down";
+            }
             return (
                 <div className="simple-quote">
                 <div class="card text-center">
                     <div class="card-body">
-                        <h5 class="card-title">{quoteData.symbol} {quoteData.companyName}</h5>
-                        <div class="card-text">
+                        <h5 class="card-title">{quoteData.symbol} ({quoteData.companyName})</h5>
+                        <div class={this.state.textClass}>
                             <div class="row">
                                 <div class="col-6">
                                     {quoteData.latestPrice}
@@ -61,7 +66,7 @@ class SimpleQuote extends React.Component {
                                 </div>
                             </div>
                         </div>
-                        <a href="#" class="btn btn-outline-success mt-3">View Full Quote</a>
+                        <a href="#" class="btn btn-outline-primary mt-3">View Full Quote</a>
                     </div>
                 </div>
             </div>
@@ -74,4 +79,4 @@ class SimpleQuote extends React.Component {
 const rootElement = document.getElementById("root");
 
 // Renders the chosen components at the root element in the HTML
-ReactDOM.render(<SimpleQuote symbol="tsla" />, rootElement);
+ReactDOM.render(<SimpleQuote symbol="amzn" />, rootElement);
