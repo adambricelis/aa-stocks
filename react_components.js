eb0163358cc8192077b751b557176fbc304ecb17
Adam Lis <adambricelis@gmail.com>
2018-10-27 14:04:13 -0400

    Built SimpleQuote component

--- react_components.js
+++ react_components.js
@@ -1,17 +1,58 @@
 class SimpleQuote extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            quoteData: [],
+            textClass: null
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://api.iextrading.com/1.0/stock/" + this.props.symbol + "/quote?filter=symbol,companyName,latestPrice,changePercent,primaryExchange,sector,open,close,marketCap,week52High,week52Low&displayPercent=true")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        quoteData: result
+                    });
+                    if (quoteData.changePercent >= 0) {
+                        textClass = "card-text up";
+                    } else {
+                        textClass = "card-text down";
+                    }
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
     render() {
-        return (
-            <div className="simple-quote">
+        const { error, isLoaded, quoteData, textClass } = this.state;
+
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div className="simple-quote">
                 <div class="card text-center">
                     <div class="card-body">
-                        <h5 class="card-title">TSLA (Tesla Inc.)</h5>
-                        <div class="card-text up">
+                        <h5 class="card-title">{quoteData.symbol} {quoteData.companyName}</h5>
+                        <div class="card-text">
                             <div class="row">
                                 <div class="col-6">
-                                    $250.00
+                                    {quoteData.latestPrice}
                                 </div>
                                 <div class="col-6">
-                                    10.00%
+                                    {quoteData.changePercent}%
                                 </div>
                             </div>
                         </div>
@@ -19,7 +60,8 @@ class SimpleQuote extends React.Component {
                     </div>
                 </div>
             </div>
-        );
+            );
+        }
     }
 }
 
@@ -27,4 +69,4 @@ class SimpleQuote extends React.Component {
 const rootElement = document.getElementById("root");
 
 // Renders the chosen components at the root element in the HTML
-ReactDOM.render(<SimpleQuote />, rootElement);
+ReactDOM.render(<SimpleQuote symbol="tsla" />, rootElement);
